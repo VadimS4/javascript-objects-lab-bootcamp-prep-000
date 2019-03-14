bfe55d509e66bd50cf05ae98ab721a15f62c1832
Vadim Stakhnyuk <vstakhnyuk@outlook.com>
2019-03-14 06:53:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ function updateObjectWithKeyAndValue(object, key, value){
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  var recipes = 'prop: 1';
+  recipes = ["prop"]
 }
